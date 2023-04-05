1387181201403d9db43def01f001f12be82d2a11
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-04-05 16:47:57 +0700

    config path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import Home from './components/pages/Home';
 import AddToDoList from "./components/pages/AddToDoList";
 
@@ -8,7 +7,7 @@ export default function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home/>} />
                 <Route path="/AddToDoList.js" element={<AddToDoList/>} />
             </Routes>
         </div>
