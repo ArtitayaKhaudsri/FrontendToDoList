96df6d3bf0ceb5aa7089ac17cdf48e180c80f385
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-04-05 20:49:56 +0700

    lay out home

--- src/components/templates/layout.js
+++ src/components/templates/layout.js
@@ -1,32 +1,12 @@
-import {BsJustify, BsJustifyLeft} from 'react-icons/bs';
 import './layout.css';
-import {useState} from "react";
 
 export default function Layout() {
-
-    const [open, setOpen] = useState(false)
-    const menu = () => {
-        if (open) {
-            return (
-                <div>
-                    <BsJustifyLeft size={28}/>
-                </div>
-
-            )
-
-        } else {
-            return <BsJustify size={28}/>
-        }
-    }
 
     return (
         <div>
             <header>
                 <h1>To do list</h1>
             </header>
-            <nav className={"button-site-bar"} onClick={() => setOpen(!open)}>
-                {menu()}
-            </nav>
         </div>
     );
 }
