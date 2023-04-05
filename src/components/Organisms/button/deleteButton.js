6fbd69c21ad6dae2e2603186b528d299fb4dc044
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-04-05 20:50:13 +0700

    delete

--- src/components/Organisms/button/deleteButton.js
+++ src/components/Organisms/button/deleteButton.js
@@ -7,6 +7,7 @@ import React, {useState} from "react";
 import DialogActions from "@mui/material/DialogActions";
 import "./deleteButton.css"
 import "./submit.css"
+import axios from "axios";
 export default function DeleteButton(props) {
 
     const [open, setOpen] = useState(false)
@@ -21,8 +22,8 @@ export default function DeleteButton(props) {
     const handleDelete = (event) => {
         event.preventDefault();
         try {
-            fetch(`http://localhost:5000/api/deleteToDoList/${props.id}`)
-                .then((response) => response.json())
+            axios.delete(`http://localhost:5000/api/toDoLists/${props.id}`)
+                .then((response) => console.log(response))
                 .catch((err) => {
                     console.log(err.message);
                 });
